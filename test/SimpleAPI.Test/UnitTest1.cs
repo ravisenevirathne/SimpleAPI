9ae72a069640ddd0ecb027e46e6ac10d93019a82
ravisenevirathne <ravi2005@gmail.com>
2021-10-06 07:10:24 +1100

    initial commit

---
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+
+namespace SimpleAPI.Test
+{
+    public class UnitTest1
+    {
+        
+
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
